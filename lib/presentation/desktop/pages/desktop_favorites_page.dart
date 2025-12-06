993181bdc4ea296c9c535477e89f69959dea6d71
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-06 14:26:23 -0700

    Add load more functionality to favorites post tap

--- lib/presentation/desktop/pages/desktop_favorites_page.dart
+++ lib/presentation/desktop/pages/desktop_favorites_page.dart
@@ -105,6 +105,11 @@ class _DesktopFavoritesPageState extends State<DesktopFavoritesPage> {
     widget.onPostTap(PostDetailArguments(
       postIds: _favorites.map((p) => p.id).toList(),
       initialIndex: index >= 0 ? index : 0,
+      hasMore: _hasMore,
+      onLoadMore: () async {
+        await _loadMore();
+        return _favorites.map((p) => p.id).toList();
+      },
     ));
   }
 
