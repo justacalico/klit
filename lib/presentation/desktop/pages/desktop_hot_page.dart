0e71424bf614cc9397515d3d8b389065328dd8b5
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-06 14:26:30 -0700

    Add load more functionality to post tap in DesktopHotPage

--- lib/presentation/desktop/pages/desktop_hot_page.dart
+++ lib/presentation/desktop/pages/desktop_hot_page.dart
@@ -55,12 +55,20 @@ class _DesktopHotPageState extends State<DesktopHotPage> {
 
   void _onPostTap(Post post) {
     final postsProvider = context.read<PostsProvider>();
+    final settingsProvider = context.read<SettingsProvider>();
     final posts = postsProvider.hotPosts;
     final index = posts.indexWhere((p) => p.id == post.id);
 
     widget.onPostTap(PostDetailArguments(
       postIds: posts.map((p) => p.id).toList(),
       initialIndex: index >= 0 ? index : 0,
+      hasMore: postsProvider.hasMoreHot,
+      onLoadMore: () async {
+        await postsProvider.loadHotPosts(
+          safeMode: settingsProvider.safeMode,
+        );
+        return postsProvider.hotPosts.map((p) => p.id).toList();
+      },
     ));
   }
 
